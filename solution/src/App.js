c1d4fd96efb5c0258679100ff7ce4334e568b185
Sheena <aries.sheen@gmail.com>
2024-04-23 09:41:49 -0700

    Defined state variables to manage form inputs, validations and table data

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,22 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // State variables to manage form inputs, validations and table data
+  const [name, setName] = useState('');
+  const [isNameTaken, setIsNameTaken] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [tableData, setTableData] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Hello</p>
       </header>
     </div>
   );
